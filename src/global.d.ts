ac81fa4e795df817c58ae9156209b8f18c7695e8
Siarhei Lunski <loonskai@gmail.com>
2021-05-16 11:22:46 +0200

    Add microsoft-cognitiveservices-speech-sdk

--- src/global.d.ts
+++ src/global.d.ts
@@ -2,6 +2,7 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       BOT_TOKEN: string;
+      SUBSCRIPTION_KEY: string;
     }
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,54 @@
+import { Transform, pipeline } from 'stream';
+import { promisify } from 'util';
 import { Telegraf } from 'telegraf';
+import { SpeechConfig, AudioConfig, AudioInputStream, SpeechRecognizer } from 'microsoft-cognitiveservices-speech-sdk';
 import axios from 'axios';
 import * as dotenv from 'dotenv';
-import { pipeline } from 'stream';
-import * as fs from 'fs';
+
 dotenv.config();
 
+const promisePipeline = promisify(pipeline);
 const bot = new Telegraf(process.env.BOT_TOKEN);
+
+const pushStream = AudioInputStream.createPushStream();
+const speechConfig = SpeechConfig.fromSubscription(process.env.SUBSCRIPTION_KEY, 'eastus');
+speechConfig.speechRecognitionLanguage = 'en-US';
 
 bot.on('text', ctx => {
   ctx.telegram.sendMessage(ctx.message.chat.id, `Hello ${ctx.state.role}`);
 });
 
 bot.on('voice', async ctx => {
-  console.log(ctx.message.voice);
-  const { href: fileUrl } = await ctx.telegram.getFileLink(ctx.message.voice.file_id);
-  const res = await axios(fileUrl, { responseType: 'stream' });
-  pipeline(
-    res.data,
-    fs.createWriteStream('test.ogg'),
-    err => {
-      console.error(err);
-    }
-  );
+  try {
+    const { href: fileUrl } = await ctx.telegram.getFileLink(ctx.message.voice.file_id);
+    const res = await axios(fileUrl, { responseType: 'stream' });
+
+    await promisePipeline(
+      res.data,
+      new Transform({
+        transform(arrayBuffer, encoding, callback) {
+          console.log(arrayBuffer);
+          pushStream.write(arrayBuffer.slice());
+          callback();
+        },
+      })
+    );
+    pushStream.close();
+
+    const audioConfig = AudioConfig.fromStreamInput(pushStream);
+    const recognizer = new SpeechRecognizer(speechConfig, audioConfig);
+    recognizer.recognizeOnceAsync((result) => {
+      console.log(result);
+      console.log(result.text);
+      recognizer.close();
+    }, err => {
+      console.trace('Error', err);
+      recognizer.close();
+    });
+  } catch (error) {
+    console.log(error);
+    ctx.telegram.sendMessage(ctx.message.chat.id, 'Something went wrong');
+  }
 });
 
 bot.launch();
